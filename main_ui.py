c2bb4a501398c4216b9544079ea3ffd576410537
LKDbigpig214 <qiudeliang214@163.com>
2022-01-03 21:46:23 +0800

    first version

---
+++ main_ui.py
@@ -0,0 +1,55 @@
+import os
+import datetime
+
+import PySimpleGUI as sg
+
+from processor import Processor
+
+
+output_path = os.path.join(os.getcwd(), 'output')
+this_year = datetime.datetime.now().year - 1
+years = list(range(this_year+2, this_year-3, -1))
+
+ic = sg.InputCombo(values=years, default_value=this_year-1,
+                   disabled=True)
+
+layout = [[sg.Text('Filename'),
+           sg.InputCombo(values=years, default_value=this_year,
+                         enable_events=True, key='year'),
+           sg.Input(),
+           sg.FileBrowse()],
+          [sg.Text('Filename'),
+           ic,
+           sg.Input(),
+           sg.FileBrowse()],
+          [sg.Text('Output'),
+           sg.Input(default_text=output_path, key='output')],
+          [sg.OK()]]
+
+window = sg.Window('EHelper', layout)
+
+while True:
+    event, values = window.read()
+    if event is None:
+        break
+    if event == 'year':
+        ic.update(values['year']-1)
+    if event == 'OK':
+        if not values['Browse'] or not values['Browse0']:
+            sg.popup('请选择两个文件')
+            continue
+        p = Processor(values['Browse'], values['Browse0'],
+                      values['year'], values['output'])
+        # p = Processor('C:/Users/Lenovo/Downloads/2021树德小学.xls',
+        #               'C:/Users/Lenovo/Downloads/2020决算表.xls',
+        #               values['year'], values['output'])
+        try:
+            p.plot_z01()
+            p.plot_z01_pie()
+            p.plot_cs02()
+            p.plot_cs02_1()
+        except Exception as exc:
+            sg.popup(str(exc))
+        p.close()
+
+window.close()
---
+++ processor/__init__.py
@@ -0,0 +1,44 @@
+from .parser import Parser
+from .output import Output
+
+
+class Processor:
+    def __init__(self, file1=None,
+                 file2=None,
+                 year=None,
+                 save_path=None):
+        self.file1 = file1
+        self.file2 = file2
+        self.wb1 = Parser(file1)
+        self.wb2 = Parser(file2)
+        self.year = year
+        self.output = Output(save_path)
+
+    def close(self):
+        self.wb1.close()
+        self.wb2.close()
+
+    def plot_z01(self):
+        x = [self.year, self.year-1]
+        y = [self.wb1.get_z01(),
+             self.wb2.get_z01()]
+        self.output.plot_z01(x, y)
+
+    def plot_z01_pie(self):
+        data, ingredients = self.wb1.get_z01_pie()
+        self.output.plot_pie(data, ingredients,
+                             '图4：收入决算结构图\n单位（元）')
+        data, ingredients = self.wb1.get_z01_pie_0()
+        self.output.plot_pie(data, ingredients,
+                             '图5：支出决算结构图\n单位（元）')
+
+    def plot_cs02(self):
+        data1 = self.wb1.get_cs02()
+        data2 = self.wb2.get_cs02()
+        self.output.plot_cs02(data1, data2, self.year)
+
+    def plot_cs02_1(self):
+        x = [self.year, self.year-1]
+        y = [self.wb1.get_cs02_1(),
+             self.wb2.get_cs02_1()]
+        self.output.plot_cs02_1(x, y)
